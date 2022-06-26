d113ca6948fde84fc16ee471fbb6e200bf2e2651
wangpeng <993080086@qq.com>
2022-06-26 23:53:15 +0800

    修改报错；

--- src/servers/api/user.js
+++ src/servers/api/user.js
@@ -1,36 +1,36 @@
 import Request from '../request'
 
 export default {
-	apis: {
-		banner: '/banner', // banner图
-		recommendSongList: '/personalized' // 推荐歌单
-	},
-	/**
-	* 获取banner图
-	* @param {Number} type 类型
-	*/
-	banner(type = 0) {
-		return Request({
-			url: this.apis.banner,
-			method: 'post',
-			data: {
-				type
-			},
-			notLoad: true
-		})
-	},
-	/**
-	* 推荐歌单
-	* @param {Number} limit 数量
-	*/
-	recommendSongList(limit = 18) {
-		return Request({
-			url: this.apis.recommendSongList,
-			method: 'post',
-			data: {
-				limit
-			},
-			notLoad: true
-		})
-	}
+  apis: {
+    banner: '/banner', // banner图
+    recommendSongList: '/personalized' // 推荐歌单
+  },
+  /**
+  * 获取banner图
+  * @param {Number} type 类型
+  */
+  banner(type = 0) {
+    return Request({
+      url: this.apis.banner,
+      method: 'post',
+      data: {
+        type
+      },
+      notLoad: true
+    })
+  },
+  /**
+  * 推荐歌单
+  * @param {Number} limit 数量
+  */
+  recommendSongList(limit = 18) {
+    return Request({
+      url: this.apis.recommendSongList,
+      method: 'post',
+      data: {
+        limit
+      },
+      notLoad: true
+    })
+  }
 }
--- src/servers/request.js
+++ src/servers/request.js
@@ -1,95 +1,95 @@
 import axios from 'axios'
 import qs from 'qs'
 import Loading from '@/utils/loading'
+import { toast } from '@/utils/toast'
 
 axios.defaults.withCredentials = true
 
 const RequestInfo = {
-	count: 0,
-	requestList: [],
-	times: null,
-	showLoading() {
-		try {
-			if (RequestInfo.count === 0) {
-				RequestInfo.times = setTimeout(() => {
-					console.log('等待时间超过500ms，开启Loading')
-					Loading.show()
-					clearTimeout(RequestInfo.times)
-					RequestInfo.times = null
-				}, 500)
-			}
-			RequestInfo.count++
-		} catch (error) {
-			console.warn(error)
-		}
-	},
-	hideLoading() {
-		try {
-			RequestInfo.count--
-			if (RequestInfo.count <= 0) RequestInfo.count = 0
-			if (RequestInfo.count === 0) {
-				Loading.hide()
-				RequestInfo.times && clearTimeout(RequestInfo.times)
-				RequestInfo.times = null
-			}
-		} catch (error) {
-			console.warn(error)
-		}
-	}
+  count: 0,
+  requestList: [],
+  times: null,
+  showLoading() {
+    try {
+      if (RequestInfo.count === 0) {
+        RequestInfo.times = setTimeout(() => {
+          console.log('等待时间超过500ms，开启Loading')
+          Loading.show()
+          clearTimeout(RequestInfo.times)
+          RequestInfo.times = null
+        }, 500)
+      }
+      RequestInfo.count++
+    } catch (error) {
+      console.warn(error)
+    }
+  },
+  hideLoading() {
+    try {
+      RequestInfo.count--
+      if (RequestInfo.count <= 0) RequestInfo.count = 0
+      if (RequestInfo.count === 0) {
+        Loading.hide()
+        RequestInfo.times && clearTimeout(RequestInfo.times)
+        RequestInfo.times = null
+      }
+    } catch (error) {
+      console.warn(error)
+    }
+  }
 }
 
 // axios实例
 const instance = axios.create({
-	timeout: 10 * 1000,
-	headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  timeout: 10 * 1000,
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 })
 
 // 请求拦截器
 instance.interceptors.request.use(
-	config => {
-		if (config.method === 'post') {
-			config.data = qs.stringify(config.data)
-		}
-		if (!config.notLoad) {
-			RequestInfo.showLoading()
-		}
-		return config
-	},
-	error => {
-		return Promise.reject(error)
-	}
+  config => {
+    if (config.method === 'post') {
+      config.data = qs.stringify(config.data)
+    }
+    if (!config.notLoad) {
+      RequestInfo.showLoading()
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
 )
 
 // 响应拦截器
 instance.interceptors.response.use(
-	res => {
-		RequestInfo.hideLoading()
-		return res.data
-	},
-	error => {
-		// 请求失败
-		RequestInfo.hideLoading()
-		console.log(error)
-		try {
-			const errorResponse = error.response
-			const status = (errorResponse.status || 0) * 1
-			switch (status) {
-			case 500:
-				TOAST.error('服务器不堪重负跑路了')
-				break
-			case 404:
-				TOAST.error('接口不存在')
-				break
-			default:
-				TOAST.error(errorResponse.data.message || '502 数据请求失败啦 ~')
-			}
-			return Promise.reject(errorResponse)
-		}	catch (e) {
-			TOAST.error('哦豁，网络开小差啦 ~')
-			const ORRER = new Error('哦豁，网络开小差啦 ~')
-			return Promise.reject(ORRER)
-		}
-	}
+  res => {
+    RequestInfo.hideLoading()
+    return res.data
+  },
+  error => {
+    // 请求失败
+    RequestInfo.hideLoading()
+    try {
+      const errorResponse = error.response
+      const status = (errorResponse.status || 0) * 1
+      switch (status) {
+        case 500:
+          toast('服务器不堪重负跑路了', 1)
+          break
+        case 404:
+          toast('接口不存在', 1)
+          break
+        default:
+          toast(errorResponse.data.message || '502 数据请求失败啦 ~', 1)
+      }
+      return Promise.reject(errorResponse)
+    } catch (e) {
+      toast('哦豁，网络开小差啦 ~', 1)
+      const ORRER = new Error('哦豁，网络开小差啦 ~')
+      return Promise.reject(ORRER)
+    }
+  }
 )
 
 export default instance
